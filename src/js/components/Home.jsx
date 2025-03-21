85b06ca2463fc1fb46cf59d2f92d9817dbd6f8f0
AlleriaDev <142839879+AlleriaDev@users.noreply.github.com>
2025-03-22 00:26:10 +0100

    Added: Footer, Message for empty list, task left counter, some styles

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,8 +7,9 @@ import TodoList from "./TodoList";
 const Home = () => {
   return (
     <div className="text-center">
-      <h1 className="text-center mt-5">Todo List</h1>
+      <h1 className="text-center mt-5">TODO LIST</h1>
       <TodoList />
+      <div className="footer">Todo List Challenge 2025 &copy;</div>
     </div>
   );
 };
--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,52 +1,78 @@
 import React, { useState } from "react";
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([
-    "Testing my app",
-    "Hating my own code",
-    "Prayin to god of coding",
-  ]);
-  const [task, setTask] = useState("");
+    const [todos, setTodos] = useState([
+        "Testing my app",
+        "Hating my own code",
+        "Prayin to god of coding",
+    ]);
+    console.log(todos.length);
+    const [task, setTask] = useState("");
 
-  const addTask = (event) => {
-    event.preventDefault();
-    const newTask = [...todos];
-    newTask.push(task);
-    setTodos(newTask);
-  };
-  const removeTask = (index) => {
-    const deletedTask = [...todos];
-    deletedTask.splice(index, 1);
-    setTodos(deletedTask);
-  };
+    //Add task to list function
+    const addTask = (event) => {
+        event.preventDefault();
+        const newTask = [...todos];
+        newTask.push(task);
+        setTodos(newTask);
+        setTask("");
+    };
 
-  const taskList = todos.map((task, index) => (
-    <li className="list-group-item" key={index}>
-      {task}
-      <button
-        className="removeButton"
-        onClick={() => {
-          removeTask(index);
-        }}
-      >
-        X
-      </button>
-    </li>
-  ));
+    //Remove task from list function
+    const removeTask = (index) => {
+        const deletedTask = [...todos];
+        deletedTask.splice(index, 1);
+        setTodos(deletedTask);
+    };
 
-  return (
-    <div className="container">
-      <form className="form-control form-control-lg" onSubmit={addTask}>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="What needs to be done?"
-          onChange={(e) => setTask(e.target.value)}
-          value={task}
-        />
-      </form>
-      <ul className="list-group list-group-flush">{taskList}</ul>
-    </div>
-  );
+    //HTML task template
+    const taskList = todos.length === 0 ?
+        <li className="list-group-item shadow-sm d-flex justify-content-center">
+            There are no task to be done. You can add new tasks now!
+        </li>
+        :
+        todos.map((task, index) =>
+            <li
+                className="list-group-item shadow-sm d-flex justify-content-center"
+                key={index}
+            >
+                <div className="row col-12">
+                    <div className="col-1">{index + 1}</div>
+                    <div className="col-10">{task}</div>
+                    <div className="col-1">
+                        <button
+                            className="removeButton rounded-circle"
+                            onClick={() => {
+                                removeTask(index);
+                            }}
+                        >
+                            x
+                        </button>
+                    </div>
+                </div>
+            </li>
+
+        );
+
+    //Task form
+    return (
+        <div className="container d-flex">
+            <div className="card col-12">
+                <form className="" onSubmit={addTask}>
+                    <input
+                        type="text"
+                        className="form-control text-center"
+                        placeholder="What needs to be done?"
+                        onChange={(e) => setTask(e.target.value)}
+                        value={task}
+                    />
+                </form>
+                <ul className="list-group list-group-flush">{taskList}</ul>
+                <div className="taskFooter d-flex justify-content-start">
+                    {todos.length} task left
+                </div>
+            </div>
+        </div>
+    );
 };
 export default TodoList;
